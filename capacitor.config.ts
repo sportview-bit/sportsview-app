c0602a7cbf996b4cfd71efa53179e3db1334034e
sportview-bit <admin@sportsviewtz.com>
2026-08-09 09:50:26 +0300

    maintance 10.7

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.sportsviewtz.app',
-  appName: 'sportsviewtz',
-  webDir: 'dist'
+  appName: 'SportsViewTZ',
+  webDir: 'dist',
+  server: {
+    url: 'https://sportsviewtz.com',
+    cleartext: false,
+  },
 };
 
 export default config;
